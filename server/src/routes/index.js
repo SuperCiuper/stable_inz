465afb55884bde532be6b9b94df5f4ca73add395
SuperCiuper <szymo1212@wp.pl>
2022-10-29 21:33:28 +0200

    routes fix

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -2,8 +2,12 @@ var express = require("express");
 var router = express.Router();
 const path = require("path");
 
+router.get("/*", (req, res) => {
+	res.sendFile(path.join(__dirname, "../../../frontend/build/index.html"));
+});
+
 /* GET home page. */
-router.get("/", function (req, res, next) {
+router.get("/", (req, res) => {
 	res.render("error", { title: "Frontend not loaded" });
 });
 
